c3fdb558ce666feaa174440036f5b98e8b668b17
Matti Hirvonen <60670065+mhmattih@users.noreply.github.com>
2024-04-17 18:36:33 +0300

    Add UI design picture and add basic functions for calculations (+,-,*,/).

---
+++ calculator.js
@@ -0,0 +1,15 @@
+function add(firstValue,secondValue){
+    return firstValue + secondValue;
+}
+
+function subtract(firstValue,secondValue){
+    return firstValue - secondValue;
+}
+
+function multiply(firstValue,secondValue){
+    return firstValue * secondValue;
+}
+
+function divide(firstValue,secondValue){
+    return firstValue / secondValue;
+}
