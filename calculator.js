62d79a44debaf3295197484d5c6381445eaf6aac
Matti Hirvonen <60670065+mhmattih@users.noreply.github.com>
2024-04-17 18:47:59 +0300

    Add functions to run a calculation (operate) and check values for calc. are valid

--- calculator.js
+++ calculator.js
@@ -13,3 +13,43 @@ function multiply(firstValue,secondValue){
 function divide(firstValue,secondValue){
     return firstValue / secondValue;
 }
+
+function checkValues(firstValue,secondValue){
+    if (typeof firstValue === "number" && typeof secondValue === "number"){
+        return true;
+    }
+    else 
+    {
+        return false;
+    }   
+}
+
+function operate(firstValue,operator,secondValue){
+
+    let returnValue;
+
+    if (checkValues(firstValue,secondValue)){
+
+        switch(operator){
+            case '+':
+                returnValue = add(firstValue,secondValue);
+                break;
+            case '-':
+                returnValue = subtract(firstValue,secondValue);
+                break;
+            case '*':
+                returnValue = multiply(firstValue,secondValue);
+                break;
+            case '/':
+                returnValue = divide(firstValue,secondValue);
+                break;
+            default:
+                console.log("Unknown operator");
+        }
+    }
+    else
+    {
+        return NaN;
+    }
+    return returnValue;
+}
